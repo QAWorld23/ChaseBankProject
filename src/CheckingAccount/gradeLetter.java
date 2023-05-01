57c6d59329bcb5630c70a3b807471d8935be00c5
daveteam21 <91298376+daveteam21@users.noreply.github.com>
2023-05-01 16:57:49 -0400

    Code change and added new class into Branch23

---
+++ src/CheckingAccount/gradeLetter.java
@@ -0,0 +1,28 @@
+package CheckingAccount;
+
+public class gradeLetter {
+
+	public static void main(String[] args) {
+		// Display GPA grade letter based on score:
+
+		/*
+		 * Score: 0-1 F grade
+		 * 
+		 * Score: 2-3 B grade
+		 * 
+		 * Score: >3 A grade
+		 */
+
+		int score = 1;
+
+		if (score <= 1) {
+			System.out.println("You got F grade");
+		}
+		else if(score <= 2 && score <= 3)
+			System.out.println("You got B grade");
+		else {
+			System.out.println("You got A!!!! You score higher than 3");
+		}
+	}
+
+}
